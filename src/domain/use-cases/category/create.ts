0f6deba30b1542ff069864e092d8999fe6a5c8b2
Ruann3res <ruanpablo1218@gmail.com>
2024-03-11 22:04:03 -0300

    feat: use case to create an category

---
+++ src/domain/use-cases/category/create.ts
@@ -0,0 +1,18 @@
+import { DataAlreadyExistsError } from "@/domain/entities/errors";
+import { CategoryRepository } from "@/infrastructure/repository";
+
+
+export type CategoryCreateUseCase = (params: { name: string }) => Promise<void>;
+
+type Setup = (categoryRepository: CategoryRepository) => CategoryCreateUseCase;
+
+export const setupCategoryCreateUseCase: Setup = (categoryRepository) => async (params) => {
+    const { name } = params;
+
+    const categoryAlreadyExists = await categoryRepository.getByName(name);
+    if (categoryAlreadyExists) {
+        throw new DataAlreadyExistsError(`JA EXISTE UMA CATEGORIA '${name}'`)
+    }
+
+    await categoryRepository.create(name);
+};
